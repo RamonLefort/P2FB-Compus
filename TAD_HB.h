aba75e0fbed9ecdb93b3bb617fe4094953d0ebd3
Ramon Lefort <ramon.lefort@students.salle.url.edu>
2026-05-12 13:11:45 +0200

    Enhance TAD_HB.h with function documentation
    Added comments to function declarations for clarity.

--- TAD_HB.h
+++ TAD_HB.h
@@ -2,9 +2,15 @@
 #define	TAD_HB_H
 
 void HB_Init();
+//Pre: -
+//Post: Inicializa el LED HeartBeat
 
 void HB_Motor();
+//Pre: -
+//Post: Motor del HeartBeat, se encarga de ir ajustando la luminosidad mediante un PWM
 
 void HB_setRebellion(unsigned char value);
+//Pre: 0 <= value <= 1
+//Post: Indica el inicio o finalización una rebelión
 
 #endif	/* TAD_HB_H */
