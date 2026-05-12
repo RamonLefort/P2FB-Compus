cc3a00a69ca321b20875d35815addcfda321d3bf
Ramon Lefort <ramon.lefort@students.salle.url.edu>
2026-05-12 13:13:13 +0200

    Update TAD_IFC.h

--- TAD_IFC.h
+++ TAD_IFC.h
@@ -2,10 +2,15 @@
 #define	TAD_IFC_H
 
 void IFC_Init();
+//Pre: -
+//Post: Inicializa la interficie
 
 void IFC_Motor();
+//Pre: -
+//Post: Motor de la interficie, se encarga de capturar y parsear los comandos y de realizar las acciones necesarias con ellos
 
 char* IFC_GetFarmName();
+//Pre: -
+//Post: Devuelve el nombre de la granja
 
 #endif	/* TAD_IFC_H */
-
