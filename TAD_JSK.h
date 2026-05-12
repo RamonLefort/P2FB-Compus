2770b8df748ba88609b50620f8bcc5d61ca959bc
Ramon Lefort <ramon.lefort@students.salle.url.edu>
2026-05-12 13:14:58 +0200

    Enhance TAD_JSK.h with function documentation
    Added comments to function declarations for clarity.

--- TAD_JSK.h
+++ TAD_JSK.h
@@ -2,10 +2,16 @@
 #define	TAD_JSK_H
 
 void JSK_Init();
+//Pre: -
+//Post: Inicializa el joystick y el light
 
 void JSK_Motor();
+//Pre: -
+//Post: Motor del joystick y del light, va realizando conversiones de ADC
 
 char JSK_getLight();
+//Pre: -
+//Post: Devuelve el estado del sensor light, 0 si esta en estado normal o 1 si esta oscuro
 
 #endif	/* TAD_JSK_H */
 
